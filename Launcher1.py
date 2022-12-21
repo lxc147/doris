419e02cc848a36251af4d022557bd411a7aca8cc
lxc147 <jitaxiaozi5569@sina.com>
2022-12-21 11:29:09 +0800

    Update Launcher1.py

--- Launcher1.py
+++ Launcher1.py
@@ -72,8 +72,8 @@ import hashlib
 import argparse
 
 
-RAM_HOLDER_AMOUNT_base = 256000000 # reserve 512Mb of Memory
-ramholder = bytearray(RAM_HOLDER_AMOUNT_base)
+# RAM_HOLDER_AMOUNT_base = 256000000 # reserve 512Mb of Memory
+# ramholder = bytearray(RAM_HOLDER_AMOUNT_base)
 
 def DownloadSingleIPFSFile(ipfsHash, timeout_=5, max_trials_=2):
     ## constants & parameters
